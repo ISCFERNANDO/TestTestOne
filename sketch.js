69b2d48ff5fab3d9ab540c61bd605f6246242b06
Luis <iscluis@hotmail.com>
2019-07-30 00:15:36 -0500

    Add function producto and division

--- sketch.js
+++ sketch.js
@@ -6,7 +6,16 @@ function substract(a, b) {
   return a - b;
 }
 
+function product(a, b) {
+  return a * b;
+}
+
+function division(a, b) {
+  return a / b;
+}
 module.exports = {
   suma,
   substract,
+  product,
+  division,
 };
--- sketch.test.js
+++ sketch.test.js
@@ -1,4 +1,4 @@
-const { suma, substract } = require("./sketch");
+const { suma, substract, division, product } = require("./sketch");
 
 test("Add 3 + 5 to 8", () => {
   expect(suma(3, 5)).toBe(8);
@@ -6,4 +6,12 @@ test("Add 3 + 5 to 8", () => {
 
 test("Substract 3 - 8 to -5", () => {
   expect(substract(3, 8)).toBe(-5);
+});
+
+test("Division 3 / 5 to 0.6", () => {
+  expect(division(3, 5)).toBe(0.6);
+});
+
+test("Producto 3 * 5 to 15", () => {
+  expect(product(3, 5)).toBe(15);
 });
